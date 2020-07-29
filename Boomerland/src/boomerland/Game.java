4456009e123a655a92f5e6e926acd7c7f1ee69d6
pedaneous <67091311+pedaneous@users.noreply.github.com>
2020-07-29 10:26:07 -0400

    resource streaming

--- Boomerland/src/boomerland/Game.java
+++ Boomerland/src/boomerland/Game.java
@@ -1,6 +1,9 @@
 package boomerland;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
@@ -12,7 +15,7 @@ public class Game extends JPanel implements Runnable {
 	private Thread thread;
 	private boolean running = false;
 	
-	private String title;
+	private String title = "Boomerland Testing";
 	private int WIDTH = 800;
 	private int HEIGHT = 600;
 	
@@ -21,27 +24,36 @@ public class Game extends JPanel implements Runnable {
 	BufferedImage image;
 	Graphics imageGraphics;
 	
+	int x = 0, y = 0;
+	
 	public Game() {
 		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 		imageGraphics = image.createGraphics();
+		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		frame = new JFrame(title);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setContentPane(this);
+		frame.pack();
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		start();
 	}
 	
-	public void addNotify() {
-		super.addNotify();
-		if(thread == null) {
-			thread = new Thread(this);
-			thread.start();
-			running = true;
-		}
+	public synchronized void start() {
+		if(running) return;
+		running = true;
+		thread = new Thread(this);
+		thread.start();
 	}
 	
 	public synchronized void stop() {
-		if(running == true) {
-			try {
-				thread.join();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+		if(!running) return;
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 	}
 	
@@ -67,6 +79,21 @@ public class Game extends JPanel implements Runnable {
 				frames = 0;
 			}
 		}
+		stop();
+	}
+	
+	private void render() {
+		Graphics g = this.getGraphics();
+		imageGraphics.setColor(Color.black);
+		imageGraphics.fillRect(0, 0, 800, 600);
+		imageGraphics.setColor(Color.white);
+		imageGraphics.fillRect(x, y, 50, 50);
+		g.drawImage(image, 0, 0, null);
+	}
+	
+	private void update() {
+		x += 1;
+		y += 1;
 	}
 	
 	public static void main(String[] args) {
